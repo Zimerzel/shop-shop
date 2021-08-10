95ad8f79bc88721927bb254e60d6564842c38700
Imerzel <zach.imerzel@gmail.com>
2021-08-10 13:45:03 -0500

    create global store

---
+++ client/src/components/CategoryMenu/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
+
+function CategoryMenu({ setCategory }) {
+  const [state, dispatch] = useStoreContext();
+  const { categories } = state;
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            setCategory(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
